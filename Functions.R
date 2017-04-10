926dc77da9db6de62a5ffc63b3cb03d6e238c9ba
plekhovd <dan.plekhov@gmail.com>
2017-04-10 14:09:57 -0400

    Edited directory function and added others
    Directory function edited
    Pdf export
    Over function

--- Functions.R
+++ Functions.R
@@ -15,23 +15,41 @@ setwd("") # set directory
 projection = "+init=EPSG:4326"   # set projection (WGS84)
 crs = crs(projection) # create crs objection
 
-dir = dir("folder containing tiles", ".hdf") # filter through directory for .hdf files
+dir = dir("tiles_raw/", ".hdf") # filter through directory for .hdf files
 
-for(i in 1:length(dir)){  # for each file in directory, access desired raster, and write to new directory (example is for NPP)
-  sds = get_subdatasets(dir[i])
+for(i in 1:length(dir)){  # for each file in directory, access NPP raster, and write to new directory
+  sds = get_subdatasets(paste0(getwd(),"/tiles_raw/",dir[i]))
   gdal_translate(sds[2], dst_dataset = "NPP.tif", overwrite = TRUE)
   r = raster("NPP.tif")
-  r = writeRaster(r, paste0("tilesProj/NPP", i, ".tif"), overwrite = TRUE)
+  r = writeRaster(r, paste0("tiles_R/NPP", i, ".tif"), overwrite = TRUE)
 }
 
-dir2 = list.files("tilesProj", ".tif")   # filter through directory for .tif files
-rasterList = lapply(paste0("tilesProj/",dir2), raster)  #create list of rasters for each time 
+dir2 = list.files("tiles_R", ".tif")   # filter through directory for .tif files
+rasterList = lapply(paste0("tiles_R/",dir2), raster)  #create list of rasters for each time 
 rasterMerge =  do.call("merge",rasterList)   # merge all rasters to single raster
 rasterMerge = projectRaster(rasterMerge, crs = crs)
 rasterMerge = writeRaster(rasterMerge, "NPPmerge.tif", overwrite = TRUE) # export raster
 
 ###reclassify raster
-rasterMerge = reclassify(rasterMerge, c(6, Inf, NA))  # remove values greater than 6
+rasterMerge = reclassify(rasterMerge, c(6, Inf, NA))  # reclassifies values greater than 6 as NA
+
+###export .pdf of iterative outputs
+pdf("/Users/danplekhov/Desktop/maps.pdf")
+for (i in 1:5){
+plot(AFG,xlim=xy$x,ylim=xy$y, main = period)
+plot(sites[sites[[period]] ==1,], pch =1, cex=0.75, add=TRUE)
+} 
+dev.off()
+
+###over function
+library(sp)
+
+for (i in 10:32){
+period = names(sites[,i])
+table = as.data.frame(table(over(sites[sites[[period]] ==1,], watersheds)[,1]))
+analysis[[period]][analysis$watershed %in% table$Var1] = table$Freq 
+}  
+
 
 
 
